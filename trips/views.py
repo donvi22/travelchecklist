ecb04d6b9503b592e4b4838db03d2becd6e72b16
Vicent Llopis <vicent22llopis@gmail.com>
2026-08-14 13:35:12 +0200

    Añadido primera home y estructura basica de rutas

--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    # Renderiza la plantilla de inicio y la devuelve como respuesta HTTP
+    return render(request, 'trips/home.html')
