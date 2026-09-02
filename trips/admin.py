d9ae668b178baf46eb632007098cc416fe1488eb
Vicent Llopis <vicent22llopis@gmail.com>
2026-09-02 12:21:35 +0200

    CRUD completo de tareas y trips y listo para probar - 2

--- trips/admin.py
+++ trips/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from .models import Trip
+from .models import Trip, Task
 
 
 @admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
-    # Columnas visibles en la lista del admin
     list_display = ('id', 'title', 'destination', 'start_date', 'end_date', 'user')
-
-    # Campos sobre los que se puede buscar desde la caja de búsqueda
     search_fields = ('title', 'destination', 'notes', 'user__username')
-
-    # Filtros laterales
     list_filter = ('destination', 'start_date')
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'trip', 'priority', 'due_date', 'done')
+    search_fields = ('title', 'trip__title')
+    list_filter = ('priority', 'done')
